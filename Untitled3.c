a491f6828f034c7abce0774fa07399ad6618ec91
vikramjeevi <81755454+vikramjeevi@users.noreply.github.com>
2022-10-02 09:24:54 +0530

    Add files via upload

--- Untitled3.c
+++ Untitled3.c
@@ -1,1 +1,29 @@
+#include<stdio.h>
+int x=10;
+int main()
+{
+ extern int x;
+  printf("%d\n",x);
+  //so();
+  //printf("%d\n",disp());
+}
 
+//int so()
+//{
+//
+//    printf("%d\n",x);
+//}
+//int disp()
+//{
+//    int x;
+//    x=x+10;
+//    return x;
+//
+//}
+//int main()
+//{
+//    printf("%d",disp());
+//
+//    printf("%d",disp());
+//    printf("%d",disp());
+//}
